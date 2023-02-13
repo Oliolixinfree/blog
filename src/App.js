21da06e04619251fc9ba1174b005947ca64e01ad
Ivan <offlane@tut.by>
2023-02-13 21:10:24 +0300

    firebase config, routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { AddEditBlog } from './pages/AddEditBlog';
+import { Detail } from './pages/Detail';
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<AddEditBlog />} />
+          <Route path="/update/:id" element={<AddEditBlog />} />
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
